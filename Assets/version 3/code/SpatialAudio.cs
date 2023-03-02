bded47f6edf496f817cd1cf0a36f09a396837537
AEATON <eatonamy6@gmail.com>
2023-03-02 15:08:53 +0000

    added unity

---
+++ Assets/version 3/code/SpatialAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialAudio : MonoBehaviour
+{
+    public GameObject player;
+    public float maxDistance = 90.0f;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.spatialBlend = 1.0f;
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        float volumeMultiplier = 1.0f - Mathf.Clamp01(distance / maxDistance);
+        audioSource.volume = volumeMultiplier;
+    }
+}
---
+++ Assets/version 3/code/StopMusicOnEnter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMusicOnEnter : MonoBehaviour
+{
+    public GameObject musicObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AudioSource audioSource = musicObject.GetComponent<AudioSource>();
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+}
